a96722b344322b0634eeabafe81ec1c16fec5e64
Francisco Souza <fran.sf@uol.com.br>
2020-09-15 17:31:16 -0300

    Validação customizada: email não repetido na validação do cliente

---
+++ src/main/java/com/franz/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,57 @@
+package com.franz.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.franz.cursomc.domain.Cliente;
+import com.franz.cursomc.dto.ClienteDTO;
+import com.franz.cursomc.repositories.ClienteRepository;
+import com.franz.cursomc.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		//#1 inclua os testes aqui, inserindo erros na lista
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente!"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		
+		// Se os testes listados em #1 todos forem cumpridos, a lista estará vazia e o método retorna true (vazio) 
+		// se não irá retornar false (não vazio)
+		
+		return list.isEmpty();
+	}
+}
